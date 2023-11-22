67a53842db61085a7299be74c913c2f612f6b46f
snowmerak <snowmerak@outlook.com>
2023-11-23 01:56:15 +0900

    Update client.go

--- client.go
+++ client.go
@@ -1,33 +1,88 @@
 package fastshare
 
 import (
+	"encoding/binary"
 	"fmt"
+	"io"
+	"net"
 
 	"golang.org/x/sys/unix"
 )
 
 type FastShareClient struct {
-	id     int
+	shmId  int
 	length int
+	port   int
 	buffer []byte
+
+	conn net.Conn
 }
 
-func Connect(id int) (*FastShareClient, error) {
-	fs := &FastShareClient{}
-	b, err := unix.SysvShmAttach(id, 0, 0)
-	if err != nil {
-		return nil, fmt.Errorf("fast-share.Connect: unix.SysvShmAttach: %w", err)
+func NewClient(port int) *FastShareClient {
+	return &FastShareClient{
+		port: port,
 	}
-	fs.id = id
-	fs.buffer = b
-	fs.length = len(b)
-	return fs, nil
 }
 
-func (fs *FastShareClient) ID() int {
-	return fs.id
+func (fc *FastShareClient) Connect() error {
+	conn, err := net.Dial("tcp", fmt.Sprintf("localhost:%d", fc.port))
+	if err != nil {
+		return fmt.Errorf("fast-share.Connect: net.Dial: %w", err)
+	}
+	fc.conn = conn
+
+	buf := [8]byte{}
+	_, err = fc.conn.Read(buf[:])
+	if err != nil {
+		return fmt.Errorf("fast-share.Connect: net.Conn.Read: %w", err)
+	}
+
+	id := binary.BigEndian.Uint64(buf[:])
+	fc.shmId = int(id)
+
+	return nil
 }
 
-func (fs *FastShareClient) Length() int {
-	return fs.length
+func (fc *FastShareClient) Attach() error {
+	b, err := unix.SysvShmAttach(fc.shmId, 0, 0)
+	if err != nil {
+		return fmt.Errorf("fast-share.Attach: unix.SysvShmAttach: %w", err)
+	}
+	fc.buffer = b
+	fc.length = len(b)
+
+	return nil
+}
+
+func (fc *FastShareClient) ID() int {
+	return fc.shmId
+}
+
+func (fc *FastShareClient) Length() int {
+	return fc.length
+}
+
+func (fc *FastShareClient) Receive(w io.Writer) (uint32, uint32, error) {
+	header := NewHeader(0, 0)
+	if err := ReadHeaderFrom(fc.conn, header); err != nil {
+		return 0, 0, fmt.Errorf("fast-share.Receive: ReadHeaderFrom: %w", err)
+	}
+	defer DisposeHeader(header)
+
+	name := header.Name()
+	length := header.Length()
+
+	// TODO: implement
+}
+
+func (fc *FastShareClient) Close() error {
+	if err := fc.conn.Close(); err != nil {
+		return fmt.Errorf("fast-share.Close: net.Conn.Close: %w", err)
+	}
+
+	if err := unix.SysvShmDetach(fc.buffer); err != nil {
+		return fmt.Errorf("fast-share.Close: unix.SysvShmDetach: %w", err)
+	}
+
+	return nil
 }
